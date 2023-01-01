963c3eb5619fddb124710692e27a380eb9fd67b4
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2023-01-01 17:32:43 +0000

    Improved function code for "first_agency()"

--- page numbers.py
+++ page numbers.py
@@ -12,35 +12,26 @@ driver.get("https://www.agencycentral.co.uk/agencysearch/engineering/agencysearc
 #driver = webdriver.Chrome(chromedriver)                                                    # use module webdriver to enable chromedriver.exe
 #driver.get("https://www.google.com/")                                                      # opens URL associated to .get function
 
-def first_agency():
-    clickable = [
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div/div[3]/div/div[1]/a",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[2]/div/div/a",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[4]/div/div[2]/span",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[1]/input",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[2]/input",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[3]/input",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[7]/input",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[8]/div/input",
-        "/html/body/div[2]/div/div[2]/section/div/section/div/div/div[3]/div/div/div/div/div/div[1]/div[102]/div[1]/div[2]/section/div/div[2]/div[2]/div[1]/div/form/div[8]/div/input"
-    ]
+filePath1 = r"C:\Users\James Mills\Documents\GitHub\Web-Scraping-Scripts\first_agency_xpaths.txt"
+openFile1 = open(filePath1,"r")
+data1 = openFile1.read()
+xpaths = data1.split("\n")
 
-    driver.find_element(By.XPATH, clickable[0]).click()
-    driver.find_element(By.XPATH, clickable[1]).click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, clickable[2]).click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, clickable[3]).click()
-    driver.find_element(By.XPATH, clickable[3]).send_keys("Joe")
-    driver.find_element(By.XPATH, clickable[4]).click()
-    driver.find_element(By.XPATH, clickable[4]).send_keys("Blogs")
-    driver.find_element(By.XPATH, clickable[5]).click()
-    driver.find_element(By.XPATH, clickable[5]).send_keys("Engineer")
-    driver.find_element(By.XPATH, clickable[6]).click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, clickable[7]).click()
-    time.sleep(2)
-    
+filePath2 = r"C:\Users\James Mills\Documents\GitHub\Web-Scraping-Scripts\first_agency_actions.txt"
+openFile2 = open(filePath2,"r")
+data2 = openFile2.read()
+steps = data2.split("\n")
+
+def first_agency():
+    c = -1
+    for step in steps:
+        if step == "click()":
+            c = c + 1
+            time.sleep(2)
+            clickable = driver.find_element(By.XPATH, xpaths[c]).click()
+        elif step != "click()":
+            time.sleep(2)
+            infoEnter = driver.find_element(By.XPATH, xpaths[c]).send_keys(step)
     
 
 def all_other_agencies():                                           # placeholder for additional function to access rest of the URL's on this page.
