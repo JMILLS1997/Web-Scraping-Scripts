c3ef03d3003dfc8b1ebfb054154ad6ffc29dcaa1
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2023-01-22 17:49:42 +0000

    Runs through entire web page, collecting URL's

--- company_websites.py
+++ company_websites.py
@@ -1,49 +1,72 @@
 ######
 # function: opens up website for agency central and crawls through to the 1st recruitment agency website.
 # future improvements:
-#   - use of cookies instead of the function "first_agency" to minimise time spent running program on startup.
-#   - "first_agency_xpaths" file will change from full xpath to xpath relative to features on page, will improve redundancy if website is updated in future.
-#   - collect url once individual agency website reached, close tab, repeat for entire page.
-#   - click on next page once all data for current agencies on page is collected.
-#   - upload data into excel alongside the "company_name" script.
+#   - click on next page once all data for current agencies on page is collected
+#   - upload data into excel alongside the "company_name" script
 ######
-
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pickle
 import time
 
-chromedriver = 'C:/Users/James Mills/Documents/GitHub/Web Scraping/chromedriver.exe'       # finds chrome driver, assigns file path to variable
-driver = webdriver.Chrome(chromedriver)                                                    # use module webdriver to enable chromedriver.exe
-driver.get("https://www.agencycentral.co.uk/agencysearch/engineering/agencysearch.htm")    # opens URL associated to .get function
+url_list = []                                                                                           # blank list to contain all collected URL's. Will be added to excel at a later stage.
+branch_list = []
 
-filePath1 = r"C:\Users\James Mills\Documents\GitHub\Web-Scraping-Scripts\first_agency_xpaths.txt"
-openFile1 = open(filePath1,"r")
-data1 = openFile1.read()
-xpaths = data1.split("\n")
 
-filePath2 = r"C:\Users\James Mills\Documents\GitHub\Web-Scraping-Scripts\first_agency_actions.txt"
-openFile2 = open(filePath2,"r")
-data2 = openFile2.read()
-steps = data2.split("\n")
+filePath = r"C:\Users\James Mills\Documents\GitHub\Web-Scraping-Scripts\uk_cities_listed.txt"           # angency central requires some locational input for certain agencies, this code accesses a list of cities in UK for reference where required.
+openFile = open(filePath,"r")
+data = openFile.read()
+cities = data.split("\n")
 
-def first_agency():
-    c = -1
-    for step in steps:
-        if step == "click()":
-            c = c + 1
-            time.sleep(2)
-            clickable = driver.find_element(By.XPATH, xpaths[c]).click()
-        elif step != "click()":
-            time.sleep(2)
-            infoEnter = driver.find_element(By.XPATH, xpaths[c]).send_keys(step)
+options = webdriver.ChromeOptions()                                                                     # allows for use of ChromeDriver without exceptions flagging up or other bugs that are yet to be removed.
+options.add_experimental_option('excludeSwitches', ['enable-logging'])                      
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.agencycentral.co.uk/agencysearch/engineering/agencysearch.htm")
+
+cookies = pickle.load(open('cookies.pkl', 'rb'))                                                        # loads cookies to the browser
+for cookie in cookies:
+    driver.add_cookie(cookie)
+
+driver.refresh()                                                                                        # refresh page to implement cookies loaded previously
+
+elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="contact-button-visit-website"]')))        # collects all relevant elemants by rel. XPATH.
+
+def location():
+    time.sleep(2)
+    print(len(driver.window_handles))
+    if len(driver.window_handles) == 1:
+        try:
+            WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="agency-branch-selection"]/div[3]/div/ul/li/span[1]'))).click()        
+        finally:
+            pass
+    else:
+        pass 
+
+
+
+def agency_url():                                                                                        # function to iterate through the data collected by "elements".
+    x = 0
+    while x < 3:                                                                                         # limits loop, will remove once script ready to go through entire website.
+        for element in elements:                                                                         # for each instance of data collected in the list "elements"...        
+            element.click()                                                                          # click on the relvant element  
+            location()                                                                                                              
+            WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))                               # driver waits for number of tabs to = 2 before moving onto next operation
+            new_tab = driver.window_handles[1]                                                           # window handles is a list of the various tabs open in a browser
+            driver.switch_to.window(new_tab)                                                             # switches focus to new tab
+            url_collect = driver.current_url                                                             # collects current tab's URL
+            print(url_collect)                                                                           # for reference
+            url_list.append(url_collect)                                                                 # appends collected URL data to list "url_list"
+            driver.close()                                                                               # closes current tab
+            WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(1))                               # waits for driver to see total number of tabs = 1
+            driver.switch_to.window(driver.window_handles[0])                                            # focuses on orignal tab
+            time.sleep(3)
+            x = x + 1
+
+agency_url()
     
-
-def all_other_agencies():                                                                   # placeholder for additional function to access rest of the URL's on this page.
-    i = 0
-
-first_agency()
-
-time.sleep(30)                                                                              # keeps tab open for 20 seconds
+time.sleep(5)                                                                             # keeps tab open for 500 seconds
 driver.quit()                                                                               # closes window, ends program
 
